5938528a81050b9afd154b30d40aa4933e9eea84
Ayyoob Ajward <ajwardayyoob@gmail.com>
2024-03-05 00:49:19 +0530

    img

---
+++ src/TestExample.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TestExample implements ActionListener {
+    public static void main(String[] args) {
+
+    }
+    public void display(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
